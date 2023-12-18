923f223488990f0b4e6e318d5163d129d77febae
Daniel Fernández <danielframos09@gmail.com>
2023-12-17 20:05:30 -0600

    add: rate limiter

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -19,5 +19,5 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteHandler)
 
-	return app.recoverPanic(router)
+	return app.recoverPanic(app.rateLimit(router))
 }
